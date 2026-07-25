adc3e3f69a5af7b0ba352dd0adbe503fe93680e6
evacristo <evaristofmsenior3@gmail.com>
2026-07-25 18:59:49 -0300

    Load remote authentication and attachment sync

--- territorio-mobile-pro/pro-addon.js
+++ territorio-mobile-pro/pro-addon.js
@@ -1,1 +1,1 @@
-document.write('<script src="pro-addon-main.js?v=1"><\/script><script src="coverage-addon.js?v=1"><\/script>');
+document.write('<script src="pro-addon-main.js?v=1"><\/script><script src="coverage-addon.js?v=1"><\/script><script src="remote-auth-addon.js?v=1"><\/script>');
