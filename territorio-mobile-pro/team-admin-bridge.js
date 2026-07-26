09f939644cbe92043bd2de427013b9b189373be4
evacristo <evaristofmsenior3@gmail.com>
2026-07-26 18:35:56 -0300

    Refresh team console reliably after remote authentication

--- territorio-mobile-pro/team-admin-bridge.js
+++ territorio-mobile-pro/team-admin-bridge.js
@@ -4,15 +4,28 @@ var CT=window.CT;
 if(!CT||!CT.remoteLogin)return;
 var baseRemoteLogin=CT.remoteLogin;
 var baseRemoteLogout=CT.remoteLogout;
+
+function refreshRemoteUi(){
+  try{if(CT.renderSync)CT.renderSync()}catch(error){}
+  window.dispatchEvent(new Event('online'));
+  document.dispatchEvent(new CustomEvent('ct:remote-auth-changed'));
+}
+function refreshSequence(){
+  refreshRemoteUi();
+  setTimeout(refreshRemoteUi,250);
+  setTimeout(refreshRemoteUi,800);
+  setTimeout(refreshRemoteUi,1800);
+}
+
 CT.remoteLogin=async function(username,password){
   var result=await baseRemoteLogin(username,password);
-  window.dispatchEvent(new Event('online'));
+  refreshSequence();
   return result;
 };
 if(baseRemoteLogout){
   CT.remoteLogout=async function(){
     var result=await baseRemoteLogout();
-    window.dispatchEvent(new Event('online'));
+    refreshSequence();
     return result;
   };
 }
